d272a919b674e54e324aad1c33cbf634994f337a
lucas <l.dallier@xsensors.ai>
2022-05-09 21:42:36 -0300

    Configuring project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import styles from '../styles/home.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello World
-    </h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1 >
+        Hello World
+      </h1>
+    </>
   )
 }
