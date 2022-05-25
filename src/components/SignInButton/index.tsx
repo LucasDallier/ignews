9c492895309e4733b9e1dbfc2bbe25aead2b55c8
lucas <l.dallier@xsensors.ai>
2022-05-24 21:41:16 -0300

    Implement Stripe function not duplicated

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,29 @@
-import styles from './styles.module.scss';
-import { FiX } from 'react-icons/fi';
-
 import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react'
 
+import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
+  console.log(session)
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Lucas Dallier Arraes
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
