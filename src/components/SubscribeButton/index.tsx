3742dcf06334d549fffa632df260ac79fed2a25e
lucas <l.dallier@xsensors.ai>
2022-05-31 17:02:53 -0300

    Finish project ig.news Chapter III ignite

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -19,7 +19,7 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
       return;
     }
 
-    if (session.data.activeSubscription) {
+    if (session?.data?.activeSubscription) {
       router.push('/posts');
       return;
     }
